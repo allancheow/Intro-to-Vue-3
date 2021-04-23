72130cdfdf39fbc09e052b9783d810bf14d762e6
Allan Cheow <allan.cheow132@gmail.com>
2021-04-23 13:53:59 -0700

    addition of codes from first 5 tutorial

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Title
+
 Intro-to-Vue-3
 Code for Vue Mastery's Intro to Vue 3 course:
 <br>
 
 # Overview
+
+Introductory lessons into Vue3
 <br>
 
 # Credit
+
 Files provided by @Code-Pop
+
 - assets (folder)
 - index.html
 - main.js
-<br>
+  <br>
 
 File build by me (@allancheow)
 <br>
--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-const product = 'Socks'
+// const product = 'Socks'
+
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      description:
+        'Soft organic cotton material formed into a blissful shroud for your feet.',
+      image: './assets/images/socks_green.jpg',
+      socksURL: 'https://en.wikipedia.org/wiki/Sock',
+      inStock: true,
+      inventory: 100,
+      onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' }
+      ],
+      sizes: ['small', 'medium', 'large', 'x-large']
+    };
+  }
+});
